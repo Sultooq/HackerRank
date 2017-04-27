b7bd6aa77c14e6054286eaab964617a299935e47
Saltuk Erkan <bugraerkan97@gmail.com>
2017-04-27 11:10:02 -0400

    Python script
    Wrote a python script to get the phrasesfrom the website using the keys

---
+++ script.py
@@ -0,0 +1,16 @@
+import requests
+
+addr='https://cdn.hackerrank.com/hackerrank/static/contests/capture-the-flag/secret/secret_json/'
+f = open('keys.txt', 'r')
+out = open('phrases.txt', 'w')
+inp = f.read()
+keys = inp.strip().split('": "",\n  "')
+keys[len(keys)-1] = keys[len(keys)-1][:5]
+keys[0] = keys[0][1:]
+
+phrases = []
+for key in keys:
+    r = requests.get(addr+key+'.json')
+    phrases.append(r.json()['news_title'])
+for phrase in phrases:
+    out.write(phrase+'\n')
